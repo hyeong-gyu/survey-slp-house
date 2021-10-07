96756b98876e29323b479b577f7741169be23090
kyle.kwak <kyle.kwak@cheilpengtai.com>
2021-10-07 17:30:17 +0900

    input id change

--- survey-web/src/views/Main.vue
+++ survey-web/src/views/Main.vue
@@ -48,10 +48,10 @@
             <div class="modal-body">
                 <div class="row g-3 align-items-center">
                     <div class="col-auto">
-                        <label for="inputPassword6" class="col-form-label">{{ type.toUpperCase() }} CODE</label>
+                        <label for="survey-code" class="col-form-label">{{ type.toUpperCase() }} CODE</label>
                     </div>
                     <div class="col-auto">
-                        <input type="password" id="inputPassword6" class="form-control" aria-describedby="passwordHelpInline" @input="codeValidation($event.target.value)">
+                        <input type="password" id="survey-code" class="form-control" aria-describedby="passwordHelpInline" @input="codeValidation($event.target.value)">
                     </div>
                     <div class="col-auto">
                         <span id="passwordHelpInline" class="form-text">
